4e9ff9e46d330e0ede37b927d191a820c19bce37
RamPrakash14 <ramprakash1424@gmail.com>
2024-02-01 00:40:09 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import express from 'express'
 import AppRoutes from './routes/index.js'
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 app.use(express.json())
 app.use('/',AppRoutes)
    
